95ad86437fa1a46352804a81a7e7f9011ec94f79
jimohsegun1 <jimohsegunj@gmail.com>
2024-11-27 14:56:48 +0100

    controllers & routes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,9 @@
+const addProduct = async (req, res) => {};
+
+const listProducts = async (req, res) => {};
+
+const removeProduct = async (req, res) => {};
+
+const singleProduct = async (req, res) => {};
+
+export { listProducts, addProduct, singleProduct, removeProduct };
