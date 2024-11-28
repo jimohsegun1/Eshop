9bf1508c78a71ae6be10d2abfa985471746b5758
jimohsegun1 <jimohsegunj@gmail.com>
2024-11-28 17:26:38 +0100

    admin login

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center py-2 px-[4%] justify-between ">
+      <Link to="/" className="flex items-center space-x-1">
+        {/* <img className="w-[max(10%,80px)]" src={assets.logo} alt="" /> */}
+        <img src={assets.logo} width={50} height={50} alt="" />
+        <span className="font-bold text-xl text-black">Eshop</span>
+      </Link>
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded text-xs sm:text-sm">
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
