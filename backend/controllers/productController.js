4eae8ced7f7a03ed974e0c816709436755778a7e
jimohsegun1 <jimohsegunj@gmail.com>
2024-11-28 13:29:20 +0100

    auth & authorization

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -48,10 +48,35 @@ const addProduct = async (req, res) => {
   }
 };
 
-const listProducts = async (req, res) => {};
+const listProducts = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id)
+    res.json({ success: true, message: "Product removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId)
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message })
+  }
+};
 
 export { listProducts, addProduct, singleProduct, removeProduct };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,11 +6,13 @@ import {
   singleProduct,
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
 productRouter.post(
   "/add",
+  adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
@@ -19,8 +21,8 @@ productRouter.post(
   ]),
   addProduct
 );
-productRouter.post("/remove", removeProduct);
-productRouter.post("/single", singleProduct);
+productRouter.post("/remove", adminAuth, removeProduct);
 productRouter.get("/list", listProducts);
+productRouter.post("/single", singleProduct);
 
 export default productRouter;
