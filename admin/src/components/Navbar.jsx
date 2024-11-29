5d2c2e85b9a372fddfd0c82ceca23eaf33a91f13
jimohsegun1 <jimohsegunj@gmail.com>
2024-11-29 05:07:42 +0100

    admin: adding data

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { assets } from "../assets/assets";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setToken }) => {
   return (
     <div className="flex items-center py-2 px-[4%] justify-between ">
       <Link to="/" className="flex items-center space-x-1">
@@ -10,7 +10,10 @@ const Navbar = () => {
         <img src={assets.logo} width={50} height={50} alt="" />
         <span className="font-bold text-xl text-black">Eshop</span>
       </Link>
-      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded text-xs sm:text-sm">
+      <button
+        onClick={() => setToken("")}
+        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+      >
         Logout
       </button>
     </div>
--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,11 +1,125 @@
-import React from 'react'
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
 
 const Add = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [image1, setImage1] = useState(false);
+  const [image2, setImage2] = useState(false);
+  const [image3, setImage3] = useState(false);
+  const [image4, setImage4] = useState(false);
 
-export default Add
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("Men");
+  const [subCategory, setSubCategory] = useState("Topwear");
+  const [bestseller, setBestseller] = useState("false");
+  const [sizes, setSizes] = useState([]);
+
+  return (
+    <form className="flex flex-col w-full items-start gap-3">
+      <div>
+        <p className="mb-2">Upload Image</p>
+        <div className="flex gap-2">
+          <label className="w-20" htmlFor="image1">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image1" hidden />
+          </label>
+          <label className="w-20" htmlFor="image2">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image2" hidden />
+          </label>
+          <label className="w-20" htmlFor="image3">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image3" hidden />
+          </label>
+          <label className="w-20" htmlFor="image4">
+            <img src={assets.upload_area} alt="" />
+            <input type="file" id="image4" hidden />
+          </label>
+        </div>
+      </div>
+
+      <div className="w-full">
+        <p className="mb-2">Product name</p>
+        <input
+          className="w-full max-w-[500px] px-3 py-2"
+          type="text"
+          placeholder="Type here"
+          required
+        />
+      </div>
+      <div className="w-full">
+        <p className="mb-2">Product description</p>
+        <textarea
+          className="w-full max-w-[500px] px-3 py-2"
+          type="text"
+          placeholder="write conrent here"
+          required
+        />
+      </div>
+
+      <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
+        <div>
+          <p className="mb-2">Product category</p>
+          <select className="w-full px-3 py-2">
+            <option value="Men">Men</option>
+            <option value="Women">Women</option>
+            <option value="Kids">Kids</option>
+          </select>
+        </div>
+        <div>
+          <p className="mb-2">Sub category</p>
+          <select className="w-full px-3 py-2">
+            <option value="Topwear">Top Wear</option>
+            <option value="Bottomwear">Bottom Wear</option>
+            <option value="Winterwear">Winter Wear</option>
+          </select>
+        </div>
+        <div>
+          <p className="mb-2">Product Price</p>
+          <input
+            className="w-full px-3 py-2 sm:w-[120px]"
+            type="number"
+            placeholder="25"
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className="mb-2">Product sizes</p>
+          <div className="flex gap-3">
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer">S</p>
+            </div>
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer">M</p>
+            </div>
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer">L</p>
+            </div>
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer">XL</p>
+            </div>
+            <div>
+              <p className="bg-slate-200 px-3 py-1 cursor-pointer">XXL</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex gap-2 mt-2">
+        <input type="checkbox" id="bestseller" />
+        <label className="cursor-pointer" htmlFor="bestseller">
+          Add to best seller
+        </label>
+      </div>
+      <button
+        className="w-28 py-3 mt-4 bg-black text-white rounded"
+        type="submit"
+      >
+        ADD
+      </button>
+    </form>
+  );
+};
+
+export default Add;
